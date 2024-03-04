429720853572c96a51de34bf283eebbe75e3477a
unknown <akmalfiqhan01@gmail.com>
2024-03-04 14:07:53 +0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # tugaspropertybehavior
+# tugaspropertybehavior
---
+++ kucing.java
@@ -0,0 +1,5 @@
+public class kucing { public static void main(String[] args) {
+    System.out.println("Saya sudah berusaha,namun tak bisa");
+}
+}
+
